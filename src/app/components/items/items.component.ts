d8e2f26de0a6b17911e4e4f1bb8c81cf292f95bc
Santino0912 <ssantinomunnoz@gmail.com>
2023-06-15 22:03:53 -0300

    agregamos los primeros items de la Tienda

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-items',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
+  items: Item[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items=[
+      {
+        id: 0,
+        title: 'manzana',
+        price: 10.5,
+        quantity: 2.4,
+        completed: false,
+      },
+      {
+        id: 1,
+        title: 'pan',
+        price: 3.5,
+        quantity: 8,
+        completed: true,
+      }
+    ]
   }
 
 }

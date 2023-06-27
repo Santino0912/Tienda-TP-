c2343691daa3b797a94c20cbe8be4c1dd152c513
Santino0912 <ssantinomunnoz@gmail.com>
2023-06-27 18:27:16 -0300

    Configure el formulario y cree el servicio item

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'app-add-item',
@@ -10,10 +11,20 @@ export class AddItemComponent implements OnInit {
   title:string='';
   price:number=0;
   quantity:number=0;
+  id:number=0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    const item = new Item();
+    item.id = this.id;
+    item.title= this.title;
+    item.price= this.price;
+    item.quantity=this.quantity
+    item.completed=false
   }
 
 }
